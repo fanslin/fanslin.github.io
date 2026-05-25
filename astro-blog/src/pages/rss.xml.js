3b7207e689cd46b5139cc3a22837e799af47a05d
fanslin <nothing80@aliyun.com>
2026-05-25 17:54:36 +0800

    Add Astro blog deployment

---
+++ astro-blog/src/pages/rss.xml.js
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const posts = (await getCollection('blog', ({ data }) => !data.draft))
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+
+  return rss({
+    title: "Fanslin's Blog",
+    description: 'Fanslin 的个人技术博客，记录系统设计、支付系统、工程实践和长期思考。',
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.id}/`
+    }))
+  });
+}
